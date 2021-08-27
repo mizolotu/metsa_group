3fc6aa84ed2463c4a84dc573f99dbf048c84a6ea
mikhail <mizolotu@jyu.fi>
2021-08-27 21:33:56 +0300

    27.8.2021

--- train_prediction_models.py
+++ train_prediction_models.py
@@ -86,9 +86,34 @@ def attention_block(x, nh):
     h = tf.keras.layers.Multiply()([a, v])
     return h
 
-def att(hidden, attention_size=512):
-    hidden = attention_block(hidden, attention_size)
-    hidden = tf.keras.layers.Flatten()(hidden)
+class Attention(tf.keras.layers.Layer):
+
+    def __init__(self,**kwargs):
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.W = self.add_weight(name="att_weight",shape=(input_shape[-1], 1),initializer="normal")
+        self.b = self.add_weight(name="att_bias",shape=(input_shape[1], 1),initializer="zeros")
+        super(Attention, self).build(input_shape)
+
+    def call(self, x):
+        et=tf.squeeze(tf.tanh(tf.tensordot(x, self.W, 1) + self.b), axis=-1)
+        at=tf.math.softmax(et)
+        at=tf.expand_dims(at, axis=-1)
+        output=x * at
+        return tf.math.reduce_sum(output, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[-1])
+
+    def get_config(self):
+        return super(Attention, self).get_config()
+
+def att(hidden, nhidden=1024):
+    #hidden = attention_block(hidden, attention_size)
+    hidden = tf.keras.layers.LSTM(nhidden, return_sequences=True)(hidden)
+    hidden = Attention()(hidden)
+    #hidden = tf.keras.layers.Flatten()(hidden)
     return hidden
 
 def rnn(hidden, nhidden=640):
