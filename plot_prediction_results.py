ca4d4df17a1ad7af66e9d7da39b5f7b43a9a742f
mikhail <mizolotu@jyu.fi>
2021-09-01 20:16:13 +0300

    1.9.2021

--- plot_prediction_results.py
+++ plot_prediction_results.py
@@ -1,11 +1,9 @@
 import os
 import os.path as osp
-import sys
 
 import pandas as pd
 import numpy as np
 import argparse as arp
-import seaborn as sn
 
 from config import *
 from matplotlib import pyplot as pp
@@ -17,8 +15,18 @@ if __name__ == '__main__':
     parser = arp.ArgumentParser(description='Plot prediction errors')
     parser.add_argument('-t', '--task', help='Task', default='predict_bleach_ratio')
     parser.add_argument('-m', '--mode', help='Mode', default='development', choices=modes)
-    parser.add_argument('-e', '--extractors', help='Feature extractors', nargs='+', default=['split_mlp', 'split_cnn1'])
+    parser.add_argument('-e', '--extractors', help='Feature extractors', nargs='+', default=['split_mlp', 'split_lstm', 'split_bilstm'])
+    parser.add_argument('-l', '--lines', help='Number of lines for each dash: solid, dashed, dotted, dash-dotted', type=int, nargs='+', default=[1, 2])
     args = parser.parse_args()
+
+    # sanitize input
+
+    if args.lines is None:
+        nlines = [1 for _ in args.extractors]
+    else:
+        nlines = args.lines
+    assert len(nlines) <= 4
+    assert len(args.extractors) <= np.sum(nlines)
 
     # create output directory
 
@@ -34,14 +42,26 @@ if __name__ == '__main__':
     e_path = osp.join(results_mode_dir, prediction_errors_fname)
     p = pd.read_csv(e_path)
 
+    # plot markers
+
+    color = 'k'
+    lines = ['-', '--', ':', '-.']
+    markers = ['o', 's', 'X', '^', '<', '>', 'v', 'D']
+
     # extract data to plot
 
+    csum = np.cumsum(nlines)
     pkeys = list(p.keys())
     assert dc_combs_col_name in pkeys
     x = p[dc_combs_col_name].values
-    y = np.zeros((len(x), len(args.extractors)))
+    ys = []
+    ls = []
+    ns = []
     for j, extractor in enumerate(args.extractors):
-        i = 0
+        ns.append(extractor.split('_')[1].upper())
+        ys.append([])
+        line_i = np.where(len(ys) <= csum)[0][0]
+        ls.append(f'{color}{markers[len(ys) - 1]}{lines[line_i]}')
         for pkey in pkeys:
             spl = pkey.split('_')
             mname = '_'.join(spl[:2])
@@ -51,18 +71,20 @@ if __name__ == '__main__':
                 else:
                     fc = int(spl[2])
                     er = p[pkey].values[fc - 1]
-                y[i, j] = er
-                i += 1
+                ys[-1].append(er)
+        assert len(ys[-1]) == len(x), print('Length mismatch for x and y')
 
     # plot error
 
     fname = '_vs_'.join(args.extractors)
     fpath = osp.join(task_figures_dir, f'prediction_errors_{fname}{pdf}')
     pp.figure(figsize=(16, 12))
-    pp.plot(x, y, 'ko-')
+    for y, l, n in zip(ys, ls, ns):
+        pp.plot(x, y, l, linewidth=2, markersize=12, label=n)
     pp.xlabel('Feature class combinations', fontdict={'size': 12})
     pp.ylabel('Mean absolute error', fontdict={'size': 12})
     pp.xticks(x, fontsize=12)
     pp.yticks(fontsize=12)
+    pp.legend()
     pp.savefig(fpath, bbox_inches='tight')
     pp.close()
