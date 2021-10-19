f9815b5c9f033ddbf988af6c791ef2da780c6505
mizolotu <mizolotu@jyu.fi>
2021-10-19 13:42:56 +0300

    19.10.2021

--- common/plot.py
+++ common/plot.py
@@ -12,7 +12,25 @@ def plot_line(x, y, marker, xlabel, ylabel, fpath):
     pp.savefig(fpath, bbox_inches='tight')
     pp.close()
 
-def plot_bars(items, heights, hatches, items_for_argsort, figh, xlabel, ylabel, legend_items, legend_names, fpath, sort=False, reverse=False, xticks_rotation='horizontal', plot_png=False):
+def plot_multiple_bars(items, heights, hatches, figh, xlabel, ylabel, legend_items, legend_names, fpath, xticks_rotation='horizontal', plot_png=False, width=0.1):
+    pp.figure(figsize=(21.2, figh))
+    x = 1 + np.arange(len(items))
+    steps = np.arange(len(items)) * width
+    for s, he, ha in zip(steps, heights, hatches):
+        pp.bar(x + s, height=he, width=width, color='white', edgecolor='black', hatch=ha)
+    pp.xlabel(xlabel, fontdict={'size': 12})
+    pp.ylabel(ylabel, fontdict={'size': 12})
+    pp.xticks(x + width * (len(items) - 1) / 2, items, fontsize=12, rotation=xticks_rotation)
+    pp.yticks(fontsize=12)
+    if len(legend_items) > 0 and len(legend_names) > 0:
+        pp.legend(legend_items, legend_names, prop={'size': 12})
+    pp.savefig(fpath, bbox_inches='tight')
+    if plot_png:
+        fpath = fpath.replace('.pdf', '.png')
+        pp.savefig(fpath, bbox_inches='tight')
+    pp.close()
+
+def plot_bars(items, heights, hatches, items_for_argsort, figh, xlabel, ylabel, legend_items, legend_names, fpath, sort=False, reverse=False, xticks_rotation='horizontal', plot_png=False, width=0.2):
     if sort:
         idx = np.argsort(items_for_argsort)
         if reverse:
@@ -21,7 +39,11 @@ def plot_bars(items, heights, hatches, items_for_argsort, figh, xlabel, ylabel, 
         heights = np.array(heights)[idx].tolist()
         hatches = np.array(hatches)[idx].tolist()
     pp.figure(figsize=(21.2, figh))
-    pp.bar(items, height=heights, color='white', edgecolor='black', hatch=hatches)
+    x = 1 + np.arange(len(items))
+    steps = np.arange(len(items)) * width
+    for s, he, ha in zip(steps, heights, hatches):
+        print(x + s, he, ha)
+        pp.bar(x + s, height=he, width=width, color='white', edgecolor='black', hatch=ha)
     pp.xlabel(xlabel, fontdict={'size': 12})
     pp.ylabel(ylabel, fontdict={'size': 12})
     pp.xticks(fontsize=12, rotation=xticks_rotation)
--- plot_prediction_results.py
+++ plot_prediction_results.py
@@ -5,12 +5,10 @@ import pandas as pd
 import numpy as np
 import argparse as arp
 
-import pylab as p
-
 from config import *
 from matplotlib import pyplot as pp
 from matplotlib.patches import Patch
-from common.plot import plot_bars
+from common.plot import plot_multiple_bars
 
 if __name__ == '__main__':
 
@@ -19,7 +17,7 @@ if __name__ == '__main__':
     parser = arp.ArgumentParser(description='Plot prediction errors')
     parser.add_argument('-t', '--task', help='Task', default='predict_bleach_ratio')
     parser.add_argument('-m', '--mode', help='Mode', default='development', choices=modes)
-    parser.add_argument('-e', '--extractors', help='Feature extractors', nargs='+', default=[])
+    parser.add_argument('-e', '--legend', help='Feature extractors', nargs='+', default=['mlp', 'lstm', 'bilstm', 'cnn1lstm', 'cnn1'])
     args = parser.parse_args()
 
     # create output directory
@@ -39,6 +37,8 @@ if __name__ == '__main__':
     p_mean = pd.read_csv(e_mean_path)
     p_max = pd.read_csv(e_max_path)
     p_results = pd.read_csv(r_path)
+    p_mean_keys = list(p_mean.keys())
+    p_max_keys = list(p_max.keys())
 
     tbl_dc_combs = p_mean[dc_combs_col_name].values
     assert np.any(tbl_dc_combs == p_max[dc_combs_col_name].values)
@@ -48,48 +48,49 @@ if __name__ == '__main__':
     color = 'k'
     lines = ['-', '--', ':', '-.']
     markers = ['o', 's', 'X', '^', '<', '>', 'v', 'D']
-    unique_hatches = ['/', '\\', '|', '-', '+', 'x', 'o', 'O', '.', '*']
+    unique_hatches = ['-', '\\', '/', '.', 'o']
     models = np.sort([key for key in p_results.keys() if key not in [br_key, ts_key]])
     dcs = np.unique([int(m.split('_')[-1]) for m in models])
 
+    # plot mean errors
+
+    dc_combs = []
     for dc in dcs:
         dc_comb = ','.join([str(item) for item in np.arange(dc) + 1])
-        legend_names = [m for m in models if int(m.split('_')[-1]) == dc]
-        hatches = [unique_hatches[0] for _ in legend_names]
-        legend_items = [Patch(facecolor='white', edgecolor='black', hatch=hatch) for hatch in hatches]
+        dc_combs.append(dc_comb)
+    if args.legend is None:
+        legend_names = list(set([m.split('_')[0] for m in models]))
+    else:
+        legend_names = args.legend
+    hatches = [unique_hatches[i] for i in range(len(legend_names))]
+    legend_items = [Patch(facecolor='white', edgecolor='black', hatch=hatch) for hatch in hatches]
 
-        # plot mean errors
+    # plot mean errors
 
-        p_mean_keys = list(p_mean.keys())
-        tags, heights = [], []
-        for name in legend_names:
-            name_without_dc = '_'.join(name.split('_')[:-1])
-            tags.append(' '.join(name_without_dc.split('_')))
-            assert name_without_dc in p_mean_keys
+    heights = []
+    for name in legend_names:
+        heights.append([])
+        for dc_comb in dc_combs:
             idx = np.where(tbl_dc_combs == dc_comb)[0]
             assert len(idx) == 1
             idx = idx[0]
-            heights.append(p_mean[name_without_dc].values[idx])
+            heights[-1].append(p_mean[name].values[idx])
 
-        fpath = osp.join(task_figures_dir, f'mean_prediction_errors_{dc}.pdf')
-        figh = 8
-        plot_bars(tags, heights, hatches, heights, figh, 'Model', 'Mean error', legend_items, legend_names, fpath, sort=True, reverse=False, plot_png=False)
+    fpath = osp.join(task_figures_dir, f'mean_prediction_errors.pdf')
+    figh = 8
+    plot_multiple_bars(dc_combs, heights, hatches, figh, 'Feature delay class combinations', 'Mean error', legend_items, legend_names, fpath, plot_png=False)
 
-        # plot max errors
+    # plot max errors
 
-        p_mean_keys = list(p_max.keys())
-        tags, heights = [], []
-        for name in legend_names:
-            name_without_dc = '_'.join(name.split('_')[:-1])
-            tags.append(' '.join(name_without_dc.split('_')))
-            assert name_without_dc in p_mean_keys
+    heights = []
+    for name in legend_names:
+        heights.append([])
+        for dc_comb in dc_combs:
             idx = np.where(tbl_dc_combs == dc_comb)[0]
             assert len(idx) == 1
             idx = idx[0]
-            heights.append(p_max[name_without_dc].values[idx])
+            heights[-1].append(p_max[name].values[idx])
 
-        fpath = osp.join(task_figures_dir, f'max_prediction_errors_{dc}.pdf')
-        figh = 8
-        plot_bars(tags, heights, hatches, heights, figh, 'Model', 'Max error', [], [], fpath, sort=True, reverse=False, plot_png=False)
-
-
+    fpath = osp.join(task_figures_dir, f'max_prediction_errors.pdf')
+    figh = 8
+    plot_multiple_bars(dc_combs, heights, hatches, figh, 'Feature delay class combinations', 'Max error', legend_items, legend_names, fpath, plot_png=False)
